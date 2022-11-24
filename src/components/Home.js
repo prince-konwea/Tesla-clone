1cf44d6611abc8a9d3b1b5243759227e68a3ee9d
prince-konwea <kokodecontee123@gmail.com>
2022-11-24 22:25:10 +0100

    styling home components with styled-components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import Section from './Section';
 
 function Home() {
   return (
-    <div>Home</div>
+    <Container>
+      <Section />
+    </Container>
   )
 }
+
+const Container= styled.div`
+  height:
+`
 
 export default Home;
